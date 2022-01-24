87c457d7730eb0f5fb76b090584e6d0e72eb0120
Ender Puentes <endpuent@gmail.com>
2022-01-24 20:41:24 -0300

    Agregado recurso para obtener el arbol de categorias

--- app/api/index.js
+++ app/api/index.js
@@ -29,7 +29,6 @@ app.get("/products", async (req, res) => {
   const responseDollar = await axios.get("dollar");
   const dataDollar = responseDollar.data;
   // Modificamos el array
-  let products = [];
   dataProducts.products.forEach((product, index) => {
     // Creamos una copia del item actual
     let item = dataProducts.products[index];
@@ -41,7 +40,6 @@ app.get("/products", async (req, res) => {
     item.originalPriceDollar = Number(
       parseFloat(product.originalPrice / dataDollar.rate).toFixed(2)
     );
-    // Reemplazamos la estension de la foto
     item.photo =
       product.photo.substr(0, product.photo.lastIndexOf(".")) + ".webp";
     // Actualizamos el item actual con la copia modificada
@@ -66,7 +64,16 @@ app.get("/categories", async (req, res) => {
 
 app.get("/categories_tree", async (req, res) => {
   const response = await axios.get("categories");
-  res.send(response.data);
+  const data = response.data;
+  let orderTree = (list, parent_id) => {
+    return list
+      .filter((n) => n.parent_id == parent_id)
+      .map((n) => {
+        n.subcategories = orderTree(list, n.id);
+        return n;
+      });
+  };
+  res.send({ categories: orderTree(data.categories, null) });
 });
 
 /**
@@ -75,7 +82,7 @@ app.get("/categories_tree", async (req, res) => {
 
 app.get("/dollar", async (req, res) => {
   const response = await axios.get("dollar");
-  res.send(response.data);
+  res.send(response.data.categories);
 });
 
 module.exports = app;
