29b97f356020bf8d6d0bb3390459753d09c5936e
Ender Puentes <endpuent@gmail.com>
2022-01-24 19:43:30 -0300

    Agregada api proxy en express

--- app/api/index.js
+++ app/api/index.js
@@ -1,12 +1,81 @@
 require("dotenv").config();
-import $axios from "axios";
 
-export default async function (req, res, next) {
-  const axios = $axios.create({
-    baseURL: process.env.API_URL,
+const $axios = require("axios");
+const axios = $axios.create({
+  baseURL: process.env.API_URL,
+});
+
+const express = require("express");
+const app = express();
+
+/**
+ * GET /
+ */
+
+app.get("", async (req, res) => {
+  const response = await axios.get("");
+  res.send(response.data);
+});
+
+/**
+ * GET /products
+ */
+
+app.get("/products", async (req, res) => {
+  // Obtenemos los productos
+  const responseProducts = await axios.get(req.url);
+  const dataProducts = responseProducts.data;
+  // Obtenemos la cotización del dolár
+  const responseDollar = await axios.get("dollar");
+  const dataDollar = responseDollar.data;
+  // Modificamos el array
+  let products = [];
+  dataProducts.products.forEach((product, index) => {
+    // Creamos una copia del item actual
+    let item = dataProducts.products[index];
+    // Agregamos el precio original en pesos
+    item.priceDollar = Number(
+      parseFloat(product.price / dataDollar.rate).toFixed(2)
+    );
+    // Agregamos el precio original en doláres
+    item.originalPriceDollar = Number(
+      parseFloat(product.originalPrice / dataDollar.rate).toFixed(2)
+    );
+    // Reemplazamos la estension de la foto
+    item.photo =
+      product.photo.substr(0, product.photo.lastIndexOf(".")) + ".webp";
+    // Actualizamos el item actual con la copia modificada
+    dataProducts.products[index] = item;
   });
+  // Retornamos la data
+  res.send(dataProducts);
+});
 
-  const response = await axios.get(req.url);
-  res.end(JSON.stringify(response.data));
-  next();
-}
+/**
+ * GET /categories
+ */
+
+app.get("/categories", async (req, res) => {
+  const response = await axios.get("categories");
+  res.send(response.data);
+});
+
+/**
+ * GET /categories_tree
+ */
+
+app.get("/categories_tree", async (req, res) => {
+  const response = await axios.get("categories");
+  res.send(response.data);
+});
+
+/**
+ * GET /dollar
+ */
+
+app.get("/dollar", async (req, res) => {
+  const response = await axios.get("dollar");
+  res.send(response.data);
+});
+
+module.exports = app;
