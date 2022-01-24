03886ab88d47b6f4e5afbbb7de2148a0d0ec6db5
Ender Puentes <endpuent@gmail.com>
2022-01-23 21:52:11 -0300

    Simplificado maquetado de agg/update en el modulo de producto

--- app/pages/products/partials/Module.vue
+++ app/pages/products/partials/Module.vue
@@ -4,30 +4,53 @@
       <img :src="data.photo" :alt="data.name" />
     </div>
     <div class="text">
-      <span class="title">
+      <div class="title">
         <h5>{{ data.name }}</h5>
-      </span>
-      <span class="price">
+      </div>
+      <div class="price">
         <h6 v-if="data.originalPrice > data.price">
           ${{ mixinFormatPrice(data.originalPrice) }}
         </h6>
-        <h3>${{ mixinFormatPrice(data.price) }}</h3>
-      </span>
-      <add-shopping-cart
-        :id-product="Number(data.id)"
-        :price-product="Number(data.price)"
-      />
+        <h3>
+          <template v-if="itemShoppingCart"
+            >{{ itemShoppingCart.amount }}x </template
+          >${{ mixinFormatPrice(data.price) }}
+        </h3>
+      </div>
+      <div class="add-shopping-cart">
+        <div v-if="itemShoppingCart" class="update">
+          <button
+            type="button"
+            @click="update(itemShoppingCart.amount - 1, 'update')"
+          >
+            -
+          </button>
+          <input
+            :value="itemShoppingCart.amount"
+            min="1"
+            type="number"
+            disabled
+          />
+          <button
+            type="button"
+            @click="update(itemShoppingCart.amount + 1, 'update')"
+          >
+            +
+          </button>
+        </div>
+        <div v-else class="add">
+          <button type="button" @click="update(1, 'add')">
+            Agregar al carrito
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import AddShoppingCart from "~/components/AddShoppingCart.vue";
 export default {
   name: "EzProductModule",
-  components: {
-    AddShoppingCart,
-  },
   props: {
     data: {
       type: Object,
@@ -36,8 +59,27 @@ export default {
   },
   data() {
     return {
-      amount: 1,
+      amount: 0,
     };
+  },
+  computed: {
+    itemShoppingCart() {
+      return this.$store.getters["shopping-cart/items"].find(
+        (item) => item.id === this.data.id
+      );
+    },
+  },
+  methods: {
+    async update(newAmount, action) {
+      await this.$store.dispatch("shopping-cart/update", {
+        action,
+        item: {
+          id: this.data.id,
+          price: this.data.price,
+          amount: newAmount,
+        },
+      });
+    },
   },
 };
 </script>
@@ -107,6 +149,53 @@ export default {
         font-size: 16px;
         line-height: 16px;
         @extend %sf-pro-semibold;
+      }
+    }
+    .add-shopping-cart {
+      padding: 20px 0;
+      .add {
+        button {
+          display: block;
+          margin: 0 auto;
+          width: 100%;
+          max-width: 132px;
+          height: 32px;
+          background-color: $White;
+          padding: 9px 0;
+          border: solid 1px $Blue;
+          border-radius: 3px;
+          text-align: center;
+          font-size: 13px;
+          line-height: 13px;
+          color: $Blue;
+          cursor: pointer;
+        }
+      }
+      .update {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        input {
+          display: block;
+          width: calc(100% - 64px);
+          height: 32px;
+          background-color: $White;
+          border: none;
+          text-align: center;
+          font-size: 14px;
+          line-height: 14px;
+          color: $Blue;
+        }
+        button {
+          display: block;
+          width: 32px;
+          height: 32px;
+          border: none;
+          border-radius: 3px;
+          background-color: $Blue;
+          color: $White;
+          cursor: pointer;
+        }
       }
     }
   }
