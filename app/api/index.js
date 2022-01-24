1cf554dbb2e2d3f28ab9af347707d40f7fb866aa
Ender Puentes <endpuent@gmail.com>
2022-01-24 20:44:30 -0300

    Sacados comentarios ambiguos

--- app/api/index.js
+++ app/api/index.js
@@ -22,30 +22,22 @@ app.get("", async (req, res) => {
  */
 
 app.get("/products", async (req, res) => {
-  // Obtenemos los productos
   const responseProducts = await axios.get(req.url);
   const dataProducts = responseProducts.data;
-  // Obtenemos la cotización del dolár
   const responseDollar = await axios.get("dollar");
   const dataDollar = responseDollar.data;
-  // Modificamos el array
   dataProducts.products.forEach((product, index) => {
-    // Creamos una copia del item actual
     let item = dataProducts.products[index];
-    // Agregamos el precio original en pesos
     item.priceDollar = Number(
       parseFloat(product.price / dataDollar.rate).toFixed(2)
     );
-    // Agregamos el precio original en doláres
     item.originalPriceDollar = Number(
       parseFloat(product.originalPrice / dataDollar.rate).toFixed(2)
     );
     item.photo =
       product.photo.substr(0, product.photo.lastIndexOf(".")) + ".webp";
-    // Actualizamos el item actual con la copia modificada
     dataProducts.products[index] = item;
   });
-  // Retornamos la data
   res.send(dataProducts);
 });
 
