c96154347de4a35c0cae323ea5bacc5ef670aa41
Ender Puentes <endpuent@gmail.com>
2022-01-25 11:42:45 -0300

    Implementado manejo de cache en recurso de carga lenta de productos

--- api/index.js
+++ api/index.js
@@ -6,21 +6,49 @@
 
 require("dotenv").config();
 
+const express = require("express");
+const mcache = require("memory-cache");
 const $axios = require("axios");
 const axios = $axios.create({
   baseURL: process.env.API_URL,
 });
 
-const express = require("express");
-const app = express();
+// Cache
 
+const cache = (duration) => {
+  return (req, res, next) => {
+    let key = "__express__" + req.originalUrl || req.url;
+    let cachedBody = mcache.get(key);
+    if (cachedBody) {
+      res.send(cachedBody);
+      return;
+    } else {
+      res.sendResponse = res.send;
+      res.send = (body) => {
+        mcache.put(key, body, duration * 1000);
+        res.sendResponse(body);
+      };
+      next();
+    }
+  };
+};
+
+// App
+
+const app = express();
 /**
  * GET /
  */
 
 app.get("", async (req, res) => {
-  const response = await axios.get("");
-  res.send(response.data);
+  await axios
+    .get(req.url)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 /**
@@ -28,32 +56,93 @@ app.get("", async (req, res) => {
  */
 
 app.get("/products", async (req, res) => {
-  const responseProducts = await axios.get(req.url);
-  const dataProducts = responseProducts.data;
-  const responseDollar = await axios.get("dollar");
-  const dataDollar = responseDollar.data;
-  dataProducts.products = dataProducts.products
-    .filter((product) => {
-      // Filtramos los productos del último mes
-      const dateLimit = new Date();
-      dateLimit.setMonth(dateLimit.getMonth() - 1);
-      return new Date(product.updatedAt) > dateLimit;
+  await axios
+    .get(req.url)
+    .then(async (responseProducts) => {
+      const dataProducts = responseProducts.data;
+      await axios
+        .get("dollar")
+        .then((responseDollar) => {
+          const dataDollar = responseDollar.data;
+          dataProducts.products = dataProducts.products
+            .filter((product) => {
+              // Filtramos los productos del último mes
+              const dateLimit = new Date();
+              dateLimit.setMonth(dateLimit.getMonth() - 1);
+              return new Date(product.updatedAt) > dateLimit;
+            })
+            .map((product) => {
+              // Cambiamos la extensión de la foto
+              product.photo =
+                product.photo.substr(0, product.photo.lastIndexOf(".")) +
+                ".webp";
+              // Calculamos el precio en dolares
+              product.priceDollar = Number(
+                parseFloat(product.price / dataDollar.rate).toFixed(2)
+              );
+              // Calculamos el precio original en dolares
+              product.originalPriceDollar = Number(
+                parseFloat(product.originalPrice / dataDollar.rate).toFixed(2)
+              );
+              return product;
+            });
+          res.send(dataProducts);
+        })
+        .catch((error) => {
+          res.send(error);
+        });
     })
-    .map((product) => {
-      // Cambiamos la extensión de la foto
-      product.photo =
-        product.photo.substr(0, product.photo.lastIndexOf(".")) + ".webp";
-      // Calculamos el precio en dolares
-      product.priceDollar = Number(
-        parseFloat(product.price / dataDollar.rate).toFixed(2)
-      );
-      // Calculamos el precio original en dolares
-      product.originalPriceDollar = Number(
-        parseFloat(product.originalPrice / dataDollar.rate).toFixed(2)
-      );
-      return product;
+    .catch((error) => {
+      res.send(error);
     });
-  res.send(dataProducts);
+});
+
+/**
+ * GET /show/products
+ */
+
+app.get("/slow/products", cache(3600), async (req, res) => {
+  await axios
+    .get(req.url)
+    .then(async (responseProducts) => {
+      const dataProducts = responseProducts.data;
+
+      await axios
+        .get("dollar")
+        .then((responseDollar) => {
+          const dataDollar = responseDollar.data;
+
+          dataProducts.products = dataProducts.products
+            .filter((product) => {
+              // Filtramos los productos del último mes
+              const dateLimit = new Date();
+              dateLimit.setMonth(dateLimit.getMonth() - 1);
+              return new Date(product.updatedAt) > dateLimit;
+            })
+            .map((product) => {
+              // Cambiamos la extensión de la foto
+              product.photo =
+                product.photo.substr(0, product.photo.lastIndexOf(".")) +
+                ".webp";
+              // Calculamos el precio en dolares
+              product.priceDollar = Number(
+                parseFloat(product.price / dataDollar.rate).toFixed(2)
+              );
+              // Calculamos el precio original en dolares
+              product.originalPriceDollar = Number(
+                parseFloat(product.originalPrice / dataDollar.rate).toFixed(2)
+              );
+              return product;
+            });
+          res.send(dataProducts);
+        })
+        .catch((error) => {
+          res.send(error);
+        });
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 /**
@@ -61,8 +150,14 @@ app.get("/products", async (req, res) => {
  */
 
 app.get("/categories", async (req, res) => {
-  const response = await axios.get("categories");
-  res.send(response.data);
+  await axios
+    .get(req.url)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 /**
@@ -70,18 +165,23 @@ app.get("/categories", async (req, res) => {
  */
 
 app.get("/categories_tree", async (req, res) => {
-  const response = await axios.get("categories");
-  const data = response.data;
-  // Ordenamos recursivamente la data en forma de arbol
-  let orderTree = (list, parent_id) => {
-    return list
-      .filter((n) => n.parent_id == parent_id)
-      .map((n) => {
-        n.subcategories = orderTree(list, n.id);
-        return n;
-      });
-  };
-  res.send({ categories: orderTree(data.categories, null) });
+  await axios
+    .get("categories")
+    .then((response) => {
+      // Ordenamos recursivamente la data en forma de arbol
+      let orderTree = (list, parent_id) => {
+        return list
+          .filter((n) => n.parent_id == parent_id)
+          .map((n) => {
+            n.subcategories = orderTree(list, n.id);
+            return n;
+          });
+      };
+      res.send({ categories: orderTree(response.data.categories, null) });
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 /**
@@ -89,8 +189,14 @@ app.get("/categories_tree", async (req, res) => {
  */
 
 app.get("/dollar", async (req, res) => {
-  const response = await axios.get("dollar");
-  res.send(response.data.categories);
+  await axios
+    .get(req.url)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 module.exports = app;
